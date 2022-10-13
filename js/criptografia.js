e3234d81f7bc7dd0c26e473c7b57d0c319d5d36d
daniloxd2022 <113191711+daniloxd2022@users.noreply.github.com>
2022-10-12 21:46:44 -0300

    Update criptografia.js

--- js/criptografia.js
+++ js/criptografia.js
@@ -24,7 +24,7 @@ document.querySelectorAll('input').forEach((input) => {
     })
 })
 
-//aparece a barra para escolher o incremento 
+//aparece a barra para numerar o incremento 
 document.querySelectorAll('select').forEach((select) => {
     select.addEventListener('change', (event) => {
         const evento = event.target;
